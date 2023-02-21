21194d3b45387f81af054ba0dd5485bc95b9214f
Russel Arbore <rarbore2@illinois.edu>
2023-02-20 22:10:53 -0600

    remove problematic loop

--- era/src/fft.c
+++ era/src/fft.c
@@ -87,20 +87,14 @@ uint64_t bitrev_sec  = 0LL;
 uint64_t bitrev_usec = 0LL;
 #endif
 
-static unsigned int _rev (unsigned int v)
+static unsigned int _rev (unsigned int n)
 {
-  unsigned int r = v;
-  int s = sizeof(v) * CHAR_BIT - 1;
-
-  for (v >>= 1; v; v >>= 1)
-  {
-    r <<= 1;
-    r |= v & 1;
-    s--;
-  }
-  r <<= s;
-
-  return r;
+  n = (n >> 1) & 0x55555555 | (n << 1) & 0xaaaaaaaa;
+  n = (n >> 2) & 0x33333333 | (n << 2) & 0xcccccccc;
+  n = (n >> 4) & 0x0f0f0f0f | (n << 4) & 0xf0f0f0f0;
+  n = (n >> 8) & 0x00ff00ff | (n << 8) & 0xff00ff00;
+  n = (n >> 16) & 0x0000ffff | (n << 16) & 0xffff0000;
+  return n;
 }
 
 
